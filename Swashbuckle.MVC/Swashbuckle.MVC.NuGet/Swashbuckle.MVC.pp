d085ef148f30c36ade29ac50d50744b202687547
ignatandrei <ignatandrei@yahoo.com>
2016-02-09 04:59:28 +0200

    first version

---
+++ Swashbuckle.MVC/Swashbuckle.MVC.NuGet/Swashbuckle.MVC.pp
@@ -0,0 +1,16 @@
+using System.Web.Http;
+using WebActivatorEx;
+using $rootnamespace$;
+using Swashbuckle.MVC.Handler;
+using Microsoft.Web.Infrastructure;
+[assembly: PreApplicationStartMethod(typeof(SwaggerMVCConfig), "Register")]
+namespace $rootnamespace$
+{
+    public class SwaggerMVCConfig
+    {
+		public static void Register()
+        {
+			DynamicModuleUtility.RegisterModule(typeof(SwashbuckleMVCModule));
+		}
+	}
+}
